96ace903481440be9c759d079cfe8fe6513aadfe
Alan Senderovich <69826656+Alan-Senderovich@users.noreply.github.com>
2024-05-08 16:28:40 -0300

    Resolución ejercicio 3 - Lógica

--- logica/3.js
+++ logica/3.js
@@ -17,6 +17,38 @@
   que retiene el agua.
  */
 
-const waterBlocks = value => {
-  return
-}
+const waterBlocks = (value) => {
+  // Me tomo el atrevimiento de comentar cada paso para una mayor comprensión del camino hacia el resultado.
+
+  let units = 0; // Inicializamos la cantidad total de unidades de agua atrapada
+  let currentBlockHeight = 0; // Altura del bloque actual
+  let nextBlockHeight = 0; // Altura del próximo bloque
+
+  // Iteramos sobre cada bloque en el contenedor
+  value.forEach((blockHeight, index) => {
+    // Verificamos si el bloque tiene una altura negativa
+    if (blockHeight < 0) {
+      throw new Error("La altura del bloque no puede ser negativa.");
+    }
+
+    // Si no estamos en el último bloque y encontramos una nueva pared
+    if (
+      index !== value.length - 1 &&
+      (index === 0 || nextBlockHeight === blockHeight)
+    ) {
+      // Establecemos la altura del bloque actual
+      currentBlockHeight = index === 0 ? blockHeight : nextBlockHeight;
+      nextBlockHeight = Math.max(...value.slice(index + 1)); // Altura del próximo bloque
+    } else {
+      // Calculamos la altura de agua entre los bloques
+      const referenceBlockHeight = Math.min(
+        nextBlockHeight,
+        currentBlockHeight
+      );
+      const waterHeight = Math.max(0, referenceBlockHeight - blockHeight);
+      units += waterHeight; // Sumamos la altura de agua atrapada
+    }
+  });
+
+  return units;
+};
