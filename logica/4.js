80aa87b9937c195b25b029b28da7987d39d4f6ee
Alan Senderovich <69826656+Alan-Senderovich@users.noreply.github.com>
2024-05-11 11:47:08 -0300

    Resolución ejercicio 4 - Lógica

--- logica/4.js
+++ logica/4.js
@@ -1,4 +1,33 @@
 // Lee detallada mente la imagen "./code_example.png" y realiza un analisis de su funcionalidad, y como lo podrias mejorar
 //Pista, el codigo presentado handlea dos diferentes tipos de tokens (api token, session token)
 
-import image from './code_example.png'
+import image from "./code_example.png";
+
+/* 
+
+-------------------------------------------------------------------------------------------
+-Funcionalidad del Código: El componente Layout tiene la funcionalidad de gestionar la autenticación del usuario, obtener datos de inicio y productos de una API, establecer la moneda del usuario, manejar la sesión del usuario automáticamente, y proporcionar un contexto de sesión para los componentes hijos. Utiliza el hook useEffect para realizar operaciones asíncronas como la obtención de datos de la API y la gestión de la sesión del usuario.
+
+-Posibles Mejoras:
+|
+--Manejo de Errores: Se podría agregar error handlers para darle feedback al usuario y notificarlo tanto de los posibles errores en las llamadas a la API, como también problemas de conexión y errores de autenticación.
+
+--Optimización del Rendimiento: Puede haber posibles mejoras en el componente, como optimizar las llamadas a la API y un uso más eficiente del estado.
+
+--Seguridad: Se podría manejar con más seguridad la información sensible, como tokens de autenticación entre otros, evitando exponer estos datos innecesariamente. Se podrían utilizar variables de entorno para tal fin.
+
+--Documentación: Sería conveniente hacer uso de comentarios en las distintas partes del código, ayudando a la comprensión y al futuro mantenimiento del mismo.
+
+--Modularizar: El código se podría separar en distintos módulos o archivos para manejar la lógica de negocio y el manejo del estado de manera aislada y así mejorar tanto la legibilidad como también la escalabilidad del código.
+
+--Testing: Sería conveniente incluir pruebas unitarias para asegurarse el correcto funcionamiento del código en distintas partes del proyecto.
+
+
+-Optimización de Código:
+|
+--Efectos: Se podría analizar las dependencias en los efectos secundarios y así asegurarse de no estar haciendo llamadas innecesarias a la API o actualizando el estado de manera incorrecta o excesiva.
+
+--Uso de Constantes: Sería una buena práctica utilizar constantes o variables para los valores que se repiten varias veces en el componente. Por ejemplo las URL de la API entre otras.
+
+--Tokens: Ya que el código maneja dos tipos de tokens (auth_token y auth), seria bueno entender las diferencias entre ambas para gestionar de forma segura y eficiente las autenticaciones.
+*/
