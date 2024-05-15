902e543e3e8fa95fac973257b493e5170d9916f9
Alan Senderovich <69826656+Alan-Senderovich@users.noreply.github.com>
2024-05-15 12:00:55 -0300

    Implementacion RoomsDetails component

---
+++ app/components/RoomsDetails.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RoomsDetails = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="font-semibold">Detalles de las habitaciones</h3>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrs standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  );
+};
+
+export default RoomsDetails;
